0c92ccd0429fccb575fe7bdba0e2da36b405f658
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-01-02 20:50:14 +0800

    Add navigation - unprotected for now

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Layout} from 'antd';
+import LinkNav from './hoc/LinkNav';
+import 'antd/dist/antd.css';
+
+const { Header, Content } = Layout;
+
+
 
 class App extends React.Component {
+
    render() {
       return (
          <div className="App">
-            <h1>Hello World</h1>
+               <Router>
+                  <Layout className="layout">
+                     <Header>
+                        <div className="logo" />        
+                        <LinkNav />      
+                     </Header>
+                     <Content style={{ padding: '30px' }}>
+                        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                           <Switch>
+                              <Route exact path="/products" render={() => <h1>Products</h1>}/>
+                              <Route exact path="/categories" render={() => <h1>Categories</h1>}/>
+                              <Route exact path="/brands" render={() => <h1>Brands</h1>}/>
+                           </Switch>
+                        </div>
+                     </Content>
+                  </Layout>
+               </Router>
          </div>
       );
    }
+   
 }
 
 export default App;
+
+
+
---
+++ client/src/components/hoc/LinkNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Menu} from 'antd';
+import {Link, withRouter} from 'react-router-dom';
+
+const LinkNav = withRouter((props) => {
+   console.log(props.location.pathname);
+   return (
+      <Menu
+         theme="dark"
+         mode="horizontal"
+         selectedKeys={[props.location.pathname]}
+         style={{ lineHeight: '64px' }}
+         >
+         <Menu.Item key="/products"><Link to="/products">Products</Link></Menu.Item>
+         <Menu.Item key="/categories"><Link to="/categories">Categories</Link></Menu.Item>
+         <Menu.Item key="/brands"><Link to="/brands">Brands</Link></Menu.Item>
+      </Menu>
+   );
+});
+
+export default LinkNav;
